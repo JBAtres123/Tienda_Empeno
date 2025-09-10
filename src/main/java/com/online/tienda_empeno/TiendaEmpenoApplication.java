1134cbcfc6ef9366d14f9cf4cb613f8a10e133bd
Jonathann <Jonathann@JONATHAN>
2025-09-10 01:51:22 -0600

    Primer commit - proyecto Spring Boot base

---
+++ src/main/java/com/online/tienda_empeno/TiendaEmpenoApplication.java
@@ -0,0 +1,13 @@
+package com.online.tienda_empeno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEmpenoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaEmpenoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/online/tienda_empeno/TiendaEmpenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.tienda_empeno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEmpenoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
