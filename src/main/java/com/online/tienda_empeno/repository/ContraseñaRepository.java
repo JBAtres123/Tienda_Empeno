5741f8a7adc56729ec1ac814608832d87e2cc2c8
Jonathann <Jonathann@JONATHAN>
2025-10-24 18:36:45 -0600

    fix: rename ContraseñaRepository to ContraseniaRepository to avoid encoding issue

--- src/main/java/com/online/tienda_empeno/repository/ContraseñaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.online.tienda_empeno.repository;
-
-import com.online.tienda_empeno.entity.Contraseña;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContraseñaRepository extends JpaRepository<Contraseña, Integer> {
-}
-
---
+++ src/main/java/com/online/tienda_empeno/repository/ContraseñiaRepository.java
@@ -0,0 +1,8 @@
+package com.online.tienda_empeno.repository;
+
+import com.online.tienda_empeno.entity.Contrasenia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContraseñaRepository extends JpaRepository<Contrasenia, Integer> {
+}
+
